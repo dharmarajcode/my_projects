d5b497707540a6cbfbc851d0c76841b623666a42
dharmarajcode <dharmarajbarbaile553@gmail.com>
2026-09-16 21:35:16 +0530

    Add files via upload

---
+++ if.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	int age = 5;
+	if(age>6){
+	    printf("your age is greater than 6");
+	
+	}
+	
+	 else{
+	 	printf("no your age is not greater than 6");
+	 }
+	
+	return 0;
+}
